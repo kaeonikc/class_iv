1705c48513baa0480a9cb61ca9a9729dabe64161
Chakkrit Kaeonikhom <kaeonikc@login1.pri.sciama3.alces.network>
2020-07-27 11:19:21 +0100

    added install script

---
+++ class-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+APPNAME="class"
+APPVERSION="interactingv_master"
+
+echo -e '\n'
+echo  '=========================================================='
+echo  ' compiling and installing '${APPNAME}' for '${_SCIAMA_SYS}
+echo  '=========================================================='
+
+export CFLAGS=""
+
+#module load cpython/2.7.16
+
+#cd src
+make clean
+export PREFIX=$HOME/programs/${APPNAME}/${APPVERSION}/$_SCIAMA_SYS/$_SCIAMA_PYTHON
+make
+if [ $? -ne 0 ]; then
+    echo -e '\n'
+    echo '================================================='
+    echo '------------------ ERROR ------------------------'
+    echo '================================================='
+    exit 1
+fi
+#mkdir -p $PREFIX
+
+cp class $PREFIX
+cp -rfv include $PREFIX/
+cp -rfv python $PREFIX/
+ln -s `pwd`/bbn $PREFIX/bbn
+ln -s `pwd`/hyrec $PREFIX/hyrec
+make clean
+module list -t 2> $PREFIX/MODULES
+echo -e '\n'
+echo '================================================='
+echo '----------------- SUCCESS -----------------------'
+echo 'application compiled and installed in '${_SCIAMA_SYS}
+echo '================================================='
